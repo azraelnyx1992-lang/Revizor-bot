e80ef4fcce4ed9337db177a539a9eb975fe684bb
azraelnyx1992-lang <azrael.nyx1992@gmail.com>
2026-07-01 10:55:36 +0300

    Update ADMIN_ID in bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from datetime import datetime
 TOKEN = os.getenv("TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
 URL = f"https://api.telegram.org/bot{TOKEN}/"
-ADMIN_ID = 8451584336
+ADMIN_ID = 8266036993
 offset = None
 start_time = datetime.now()
 
